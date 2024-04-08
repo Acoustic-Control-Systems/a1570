ad26bf0de34456f88323bfdfdeeab37329f9d535
pylnik <pylnik@gmail.com>
2024-04-08 15:24:30 +0200

    Extended example for vector acquisition

--- SCPI_Python/receive_data_show.py
+++ SCPI_Python/receive_data_show.py
@@ -6,13 +6,16 @@ import logging
 
 from common_functions import *
 
+# set up logging
 logger = logging.getLogger()
+# create console handler and set level to info
 logger.level = logging.INFO
-
 stream_handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(stream_handler)
+
 logger.info('Start receiving data from A1570...')
 
+# ip of the device and port
 ip: str = '192.168.0.11'
 port: int = 5025
 rm = visa.ResourceManager()
@@ -28,27 +31,49 @@ while True:
     if (err_num == 0):
         break
 
+# read IDN string, it returns manufacturer, model, serial number and firmware version
+# e.g. 'ACS-Solutions GmbH,A1570,123456789,ESP 1.25 MCU 6.01.244'
 idn: str = inst.query('*IDN?')
 logger.info(idn)
+
+# set trigger to internal
+inst.write('TRIGgering:MODE INTERNAL')
+# read back trigger mode (optional)
+answ = inst.query('TRIGgering:MODE?')
+logger.info(f'Trigger mode: {answ}')
+assert 'INTERNAL' == answ, f'Failed on setting the trigger mode to INTERNAL. Received {answ}'
 
 # set internal trigger to 100 ms
 inst.write('TRIG:INT 100000 US')
 time.sleep(0.5)
+# read back trigger interval (optional)
+answ = inst.query('TRIGgering:INTerval?')
+logger.info(f'Trigger interval: {answ} seconds')
+
 # enable transmitter
 inst.write('TRAN:ENAB ON')
 time.sleep(0.5)
+# read back transmitter state (optional)
+answ = inst.query('TRAN:ENAB?')
+logger.info(f'Transmitter state: {answ}')
+
 # start measurement
 inst.write(f'STAR')
 time.sleep(0.5)
 
+# read data
 arr = inst.query_binary_values(f'FETC?', 
                                     datatype='h',
                                     is_big_endian=False,
-                                expect_termination=True,
-                                header_fmt='ieee',
+                                    expect_termination=False,
+                                    header_fmt='ieee',
                                 )
 
+# stop measurement
 inst.write(f'STOP')
+
+# close connection
+inst.close()
 
 #bytes 16, 17 is vector index
 vector_index = arr[8]
@@ -59,3 +84,6 @@ arr_vector = arr[14:]
 
 plt.plot(arr_vector)
 plt.show()
+
+# remove stream handler
+logger.removeHandler(stream_handler)
